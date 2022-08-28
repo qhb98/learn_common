e55b82c90e2f615db007de6344d6439b395264bf
qhb98 <2651311819@qq.com>
2022-08-28 16:54:47 +0800

    [add] 模拟退火

--- or/heuristic_algorithm/模拟退火/code_examples/3.py
+++ or/heuristic_algorithm/模拟退火/code_examples/3.py
@@ -5,6 +5,7 @@
 # coding:gbk
 
 """
+
 example_3:
     模拟退火算法求解背包问题
 
@@ -14,65 +15,17 @@ import random
 import math
 
 
-def cop(a, b, le):
-    # 复制函数 把b数组的值赋值a数组
-    for i in range(le):
-        a[i] = b[i]
-
-
-
-
-
-
-def get(x):
-    # 随机将背包中已经存在的物品取出
-    while 1 > 0:
-        ob = random.randint(0, m - 1)
-        if x[ob] == 1:
-            x[ob] = 0
-        break
-
-
-def put(x):  # 随机放入背包中不存在的物品
-    while 1 > 0:
-        ob = random.randint(0, m - 1)
-        if x[ob] == 0:
-            x[ob] = 1
-        break
-
-
-def slove():  # 迭代函数
-    global best, T, balance
-    test = [0] * m
-    now = 0  # 当前背包价值
-    for i in range(balance):
-        now = calc(now_way)
-        cop(test, now_way, m)
-        ob = random.randint(0, m - 1)  # 随机选取某个物品
-        if (test[ob] == 1):
-            put(test)
-            test[ob] = 0  # 在背包中则将其拿出，并加入其它物品
-        else:  # 不在背包中则直接加入或替换掉已在背包中的物品
-            if (random.random() < 0.5):
-                test[ob] = 1
-            else:
-                get(test)
-                test[ob] = 1
-        temp = calc(test)
-        if (wsum > C): continue  # 非法解则跳过
-        if temp > best: best = temp
-        cop(best_way, test, m)  # 更新全局最优
-
-        if (temp > now):
-            cop(now_way, test, m)  # 直接接受新解 
-        else:
-            g = 1.0 * (temp - now) / T
-            if (random.random() < math.exp(g)):  # 概率接受劣解
-                cop(now_way, test, m)
-
-
 def calc(things_weights, things_values, cur_res):
-    # 计算背包当前的重量和价值
+    """
+    计算当前的物品占据的背包的重量和物品的价值
+    Args:
+        things_weights: 各物品的重量列表
+        things_values: 各物品的价值列表
+        cur_res: 当前解[0 1 0...]
+
+    Returns:
+
+    """
     bag_values = 0
     bag_weights = 0
     for i in range(len(things_weights)):
@@ -81,56 +34,115 @@ def calc(things_weights, things_values, cur_res):
     return bag_weights, bag_values
 
 
-def init_res_produce(things_weights, things_values, cur_res, best_res):
+def init_res_produce(things_weights, things_values, cur_res, bag_capacity):
+    """
+    初始化函数, 用于产生随机的初始解
+    Args:
+        things_weights: 物品的重量列表
+        things_values: 物品的价值列表
+        cur_res: 当前的解
+        bag_capacity: 背包的最大重量
+
+    Returns:
+
+    """
     # 初始产生随机解
-    while 1 > 0:
+    while True:
         for i in range(len(things_weights)):
             if random.random() < 0.5:
                 cur_res[i] = 1
             else:
-                cur_res[k] = 0
-        # 计算初始解的价值
+                cur_res[i] = 0
+        # 计算初始解的占据的重量和价值
         cur_bag_weight, cur_bag_value = calc(things_weights, things_values, cur_res)
-        if wsum < C:
+        # 保证产生的是可行解
+        if cur_bag_weight < bag_capacity:
             break
-    best = calc(cur_res)
-    cop(best_way, cur_res, len(things_weights))
+    return cur_res[:]
 
 
-def simulated_annealing(weights, values, cur_tem, lamda_value, l_max, balance_number, best_res, cur_res):
-    # 初始化
-    init_res_produce(weights, values, cur_res, best_res)
-    isGood = 0
-    for i in range(k_max):
-        slove()
-        T = T * af  # 温度下降
-        if best == 295:
-            print('找到最优解:295,迭代次数', i + 1)
-            isGood = 1
-            break  # 达到最优解提前退出
+def simulated_annealing(weights, values, cur_tem, min_tem, lamda_value, l_max, cur_res, bag_capacity, best_value):
+    """
+    模拟退火求解背包问题主函数
+    Args:
+        weights: 各物品的重量列表
+        values: 各物品的价值列表
+        cur_tem: 当前温度
+        min_tem: 最低温度
+        lamda_value: 温度衰减系数
+        l_max: 固定温度下循环迭代的最大次数
+        cur_res: 当前的解
+        bag_capacity: 背包的最大重量
+        best_value: 最优解对应的价值
 
-    if isGood == 0:
-        print('只找到次优解:', best, '迭代次数', k_max)
-    print('方案为：', best_way)  # 打印方案
+    Returns:
+
+    """
+    # 初始化解
+    init_res = init_res_produce(weights, values, cur_res, bag_capacity)
+    cur_res = init_res
+    # 记录历史解
+    trend = []
+    # 记录最优解
+    best_res = None
+    while cur_tem >= min_tem:
+        for i in range(l_max):
+            cur_bag_weight, cur_bag_value = calc(weights, values, cur_res)
+            # 随机产生新解 randint(0, 1)生成的是[0, 1]范围内的整数, 注意是双闭区间
+            test_res = [random.randint(0, 1) for i in range(len(weights))]
+            choose = random.randint(0, len(weights) - 1)
+            # 在背包中则将其拿出，并加入其它物品
+            if test_res[choose] == 1:
+                test_res[choose] = 0
+            else:
+                # 不在背包中则按照概率直接加入
+                if random.random() < 0.5:
+                    test_res[choose] = 1
+            test_bag_weight, test_bag_value = calc(weights, values, test_res)
+            # 非法解则跳过
+            if test_bag_weight > bag_capacity:
+                continue
+            # 如果新解更优, 则更新解, 否则按metropolis准则更新解
+            if test_bag_value > cur_bag_value:
+                cur_res = test_res[:]
+            else:
+                # 按概率接受劣解
+                if random.random() < math.exp(- (test_bag_value - cur_bag_value) / cur_tem):
+                    cur_res = test_res[:]
+        # 温度下降
+        cur_tem *= lamda_value
+        cur_bag_weight, cur_bag_value = calc(weights, values, cur_res)
+        # 将当前的局部最优解和已知的全局最优解比较, 若更佳, 则更新全局最优
+        if cur_bag_value >= best_value:
+            best_value = cur_bag_value
+            best_res = cur_res
+        trend.append((cur_res, cur_bag_weight, cur_bag_value))
+        print(cur_tem, cur_res)
+    return trend, best_value, best_res
 
 
 if __name__ == '__main__':
+    # 背包的容量
+    C = 8
     # 物品占据的空间
-    weight = [95, 4, 60, 32, 23, 72, 80, 62, 65, 46]
+    weight = [2, 3, 5, 1, 4]
     # 物品的价值
-    value = [55, 10, 47, 5, 4, 50, 8, 61, 85, 87]
+    value = [2, 5, 8, 3, 6]
     # 当前温度
-    current_tem = 200.0
+    current_tem = 2000
+    # 最终的温度
+    final_tem = 10
     # 温度衰减系数
     lamda = 0.95
     # 迭代次数
-    k = 10
-    # 温度平衡的最大次数
-    balance_num = 100
+    k = 100
     # 物品数量
     things_number = len(weight)
-    # best_way 记录全局最优解
-    best_way = [0] * things_number
     # cur_way 记录当前解
     cur_way = [0] * things_number
-    simulated_annealing(weight, value, current_tem, lamda, k, balance_num, best_way, cur_way)
+    # 记录最优解对应的价值
+    best_res_value = 0
+    res_trend, best_result_value, best_result = simulated_annealing(weight, value, current_tem, final_tem, lamda, k,
+                                                                    cur_way, C,
+                                                                    best_res_value)
+    print("best res is: ", str(best_result))
